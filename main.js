4a5a23fa1dfe99346fc41c2d7450842a9c90e204
unknown <mariavictoriagomez1988@gmail.com>
2024-01-02 13:47:51 -0300

    menu desplegable

--- main.js
+++ main.js
@@ -8,3 +8,34 @@ botonMostrarTarjeta.addEventListener("click", function() {
         tarjeta.style.display = "none";
     }
 }); 
+
+
+function toggleMenu() {
+    let menu = document.getElementById('menu');
+    menu.classList.toggle('active');
+}
+
+// Agrega un evento al elemento li con id 'menuTrigger' para que el menú se despliegue en hover
+document.getElementById('menuTrigger').addEventListener('mouseover', toggleMenu);
+document.getElementById('menuTrigger').addEventListener('mouseout', toggleMenu); // Para ocultar el menú cuando se sale del elemento
+
+
+const text = "PIT DEODORO ROCA";
+const typingSpeed = 100; // Velocidad de escritura en milisegundos
+
+const typingEffect = () => {
+    const textElement = document.getElementById("typing-effect");
+    textElement.innerHTML = ""; // Limpia el contenido del elemento
+
+    let i = 0;
+    const typeWriter = setInterval(() => {
+        if (i < text.length) {
+            textElement.innerHTML += text.charAt(i);
+            i++;
+        } else {
+            clearInterval(typeWriter);
+        }
+    }, typingSpeed);
+};
+
+typingEffect();
